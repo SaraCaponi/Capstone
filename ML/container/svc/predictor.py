d9f740e9d79dd6981c61bcf32480656e05626b8b
Daniel Jaegers <daj6gd@mail.missouri.edu>
2020-02-26 20:58:06 -0600

    predictor script get model

--- ML/container/svc/predictor.py
+++ ML/container/svc/predictor.py
@@ -1,1 +1,24 @@
-# Predictor
+# Implements a Flask server in order to do predictions.
+
+from __future__ import print_function
+
+import os
+import pickle
+
+prefix = '/opt/ml/'
+
+model_path = os.path.join(prefix, 'model')
+
+
+class ScoringService(object):
+    model = None
+
+    @classmethod
+    def get_model(cls):
+        """Get the model object for this instance, loading it if it's not already loaded."""
+
+        if cls.model == None:
+            with open(os.path.join(model_path, 'svc-model.pkl'), 'r') as f:
+                cls.model = pickle.load(f)
+
+        return cls.model
