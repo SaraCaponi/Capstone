de991768f90a8a47e3e289edea32c23832441537
Daniel Jaegers <daj6gd@mail.missouri.edu>
2020-03-09 21:01:33 -0500

    Successfully deployed

--- svc/svc.py
+++ svc/svc.py
@@ -1,15 +1,19 @@
+
 import argparse
 import json
 import os
 import re
 from io import StringIO
+from distutils.util import strtobool
 
 import numpy as np
 import pandas as pd
 
 from sklearn.externals import joblib
+# import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
@@ -60,10 +64,11 @@ def model_fn(model_dir):
 def input_fn(request_body, request_content_type):
     """An input_fun that loads JSON into a Pandas DataFrame, and preprocesses the tweets"""
     if request_content_type == "application/json":
+        # TODO We don't really need Pandas here anymore
         df = pd.read_json(StringIO(request_body))
         # TODO Add way to provide stem parameter
         df.tweet = df.tweet.apply(lambda x: preprocess(x))
-        return df
+        return df['tweet'].to_numpy()
     else:
         raise ValueError(
             '{} is not supported by script.'.format(request_content_type))
@@ -71,13 +76,13 @@ def input_fn(request_body, request_content_type):
 
 def predict_fn(input_data, model):
     pred = model.predict(input_data)
-    pred_prob = model.predict_proba(input_data)
+    deci_func = model.decision_function(input_data)
 
     predictions = []
-    for p, pp in zip(pred, pred_prob):
+    for p, d in zip(pred, deci_func):
         predictions.append({
             'prediction': p,
-            'probability': pp
+            'probability': d
         })
 
     return {'results': predictions}
@@ -98,14 +103,15 @@ if __name__ == '__main__':
 
     # Hyperparameters from the client
     parser.add_argument('--stem', type=bool, default=False)
-    parser.add_argument('--use-idf', type=bool, default=True)
-    parser.add_argument('--smooth-idf', type=bool, default=True)
-    parser.add_argument('--sublinear-tf', type=bool, default=True)
-    parser.add_argument('--kernel', type=str, default='linear')
+    parser.add_argument('--use-idf', type=str, default='true')
+    parser.add_argument('--smooth-idf', type=str, default='true')
+    parser.add_argument('--sublinear-tf', type=str, default='true')
     parser.add_argument('--C', type=float, default=1.0)
-    parser.add_argument('--probability', type=bool, default=True)
+    parser.add_argument('--penalty', type=str, default='l2')
+    parser.add_argument('--loss', type=str, default='squared_hinge')
 
     # Data, model, and output directories
+    # TODO Remove this argument?
     # parser.add_argument('--output-data-dir', type=str,
     #                     default=os.environ.get('SM_OUTPUT_DATA_DIR'))
     parser.add_argument('--model-dir', type=str,
@@ -145,12 +151,12 @@ if __name__ == '__main__':
 
     print('Training the model')
     pipe = Pipeline([
-        ('tfidf', TfidfVectorizer(use_idf=args.use_idf,
-                                  smooth_idf=args.smooth_idf,
-                                  sublinear_tf=args.sublinear_tf)),
-        ('clf', SVC(kernel=args.kernel,
-                    C=args.C,
-                    probability=args.probability))
+        ('tfidf', TfidfVectorizer(use_idf=bool(strtobool(args.use_idf)),
+                                  smooth_idf=bool(strtobool(args.smooth_idf)),
+                                  sublinear_tf=bool(strtobool(args.sublinear_tf)))),
+        ('svc', LinearSVC(C=args.C,
+                          penalty=args.penalty,
+                          loss=args.loss))
     ])
 
     clf = pipe.fit(X_train, y_train)
@@ -158,6 +164,10 @@ if __name__ == '__main__':
     print('Print validation statistics')
     pred = clf.predict(X_test)
     # pred_prob = clf.predict_proba(X_test)
+    decision = clf.decision_function(X_test)
+
+    print(pred)
+    print(decision)
 
     # TODO Why the fuck are these the same?
     print('Accuracy: {}'.format(accuracy_score(y_test, pred)))
