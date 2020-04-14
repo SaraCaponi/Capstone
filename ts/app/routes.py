519ab30cbab2aadba402cb5e3ce462c5d108eb3d
Daniel Jaegers <daj6gd@mail.missouri.edu>
2020-04-14 13:29:22 -0500

    Display form input

--- ts/app/routes.py
+++ ts/app/routes.py
@@ -1,11 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import application
 from app.forms import SearchForm
 
-@application.route('/')
-@application.route('/index/')
+@application.route('/', methods=['GET', 'POST'])
+@application.route('/index/', methods=['GET', 'POST'])
 def home():
     form = SearchForm()
+
+    if form.validate_on_submit():
+        flash('Query {}'.format(form.query.data))
+        return render_template('index.html', form=form, data=form.query.data)
+
+
     return render_template('index.html', form=form)
 
 @application.route('/username/')
