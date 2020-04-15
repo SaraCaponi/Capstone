84343e57c48a5610dc27d1e36a2887ed88a00cf3
Sara Caponi <sbc436@mail.missouri.edu>
2020-04-14 19:27:15 -0500

    Connected front end to tweepy

---
+++ ts/app/TweepyFunctions.py
@@ -0,0 +1,170 @@
+import tweepy 
+import json
+import re
+from datetime import datetime 
+from datetime import timedelta
+from .Credentials import access_key, access_secret, consumer_key, consumer_secret
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+RATE_ERROR = 0 
+SEARCH_ERROR = 1
+stop_words = stopwords.words("english")
+stemmer = SnowballStemmer("english")
+TWEET_CLEANING_RE = r'@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+'
+
+
+#returns all tweets tweeted in the past year by given username
+def get_users_tweets(username): 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+    auth.set_access_token(access_key, access_secret) 
+
+    api = tweepy.API(auth) 
+
+    # gets the current time and subtracts 1 year to find the end time
+    current_time = datetime.now()
+    end_time = current_time - timedelta(days=365)
+
+    temp = []
+    # attempts to get users tweets, if user does not exist or api call fails will return empty array 
+    try:
+         # gets first 200 tweets
+        tweets = api.user_timeline(screen_name=username, 
+                                   count = 200,
+                                   tweet_mode="extended"
+                                   ) 
+        
+         # if there are no tweets for the user, return empty array
+        if not tweets:
+            return []
+            
+        # creates array to hold all of the tweets and appends the exsiting tweets array to it
+        all_tweets = []
+        all_tweets.extend(tweets)
+         # gets the id of the last tweet 
+        oldest_id = tweets[-1].id
+
+        # while the time of tweet of the last tweet in the array is more than the end time
+        while tweets[-1].created_at > end_time:
+            # gets the next 200 tweets starting at the last id
+            tweets = api.user_timeline(screen_name=username, 
+                           count=200,
+                           max_id = oldest_id - 1, 
+                           tweet_mode="extended"
+                           )
+            # if there are no more tweets, break the loop 
+            if len(tweets) == 0:
+                break
+            # sets the oldest_id to the id of the last tweet    
+            oldest_id = tweets[-1].id
+            # adds the tweets to the all_tweets array
+            all_tweets.extend(tweets)
+            
+    except tweepy.TweepError as err:
+         # returns error if rate limit is exceeded
+        if err.reason.find("status code = 429") > -1:
+           return RATE_ERROR   
+        #returns error if any other error, username doesnt exist, invalid syntax, user is private, no tweets in past year
+        return SEARCH_ERROR
+    
+    for tweet in all_tweets:
+        # need to do another check because the code could enter the loop but only 5 more tweets were in the last year
+        if tweet.created_at > end_time:
+            # extracts just the text
+            temp.append(tweet.full_text)
+
+    # returns array of tweets if sucessful
+    result = {"tweet": temp}
+    return result
+    
+
+
+# returns 3200 most recent tweets in the past week for given hashtag
+def get_hashtag_tweets(hashtag):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+    auth.set_access_token(access_key, access_secret) 
+    # create api call 
+    api = tweepy.API(auth ) 
+
+    tweets = []
+    try:
+        #gets 100 tweets at a time until there are 3200 total
+        for tweet in tweepy.Cursor(api.search,q="#"+hashtag,
+                               count=100,
+                               lang="en",
+                               tweet_mode="extended"
+                               ).items(3200):
+            #appends the tweets to the arry 
+            tweets.append(tweet.full_text)
+    
+    
+    except tweepy.TweepError as err:
+         # returns error if rate limit is exceeded
+        if err.reason.find("status code = 429") > -1:
+           return RATE_ERROR   
+        #returns error if any other error, hashtag doesnt exist, invalid syntax, no tweets avalible in past 7 days, ect
+        return SEARCH_ERROR
+
+
+    # if sucessful returns tweets 
+    if tweets:
+        result = {"tweet": tweets}
+        return result
+    return []
+
+
+
+#runs preprocessing algorithm on tweets
+def preprocess(tweet, stem=False):
+    """Preprocesses one tweet"""
+    tweet = re.sub(TWEET_CLEANING_RE, ' ', str(tweet).lower()).strip()
+    tokens = []
+    for token in tweet.split():
+        if token not in stop_words:
+            if stem:
+                tokens.append(stemmer.stem(token))
+            else:
+                tokens.append(token)
+    return " ".join(tokens)
+
+
+
+#aggregates the response from the sentiment algorithm and returns a result object with score, most pos index, and most neg index 
+def aggregateData(data):
+    # declaring and initializing return varaible 
+    result = {'score': 0, 'posIndex': 0 , 'negIndex': 0}
+
+    pos = 0
+    neg = 0
+    numOfTweets = 0
+    
+    # loops through the results array in the json response
+    for x in data['results']:   
+        numOfTweets += 1            # counts the number of tweets
+        if x['prediction'] == "POSITIVE":   
+            pos +=1          # counts the number of tweets that have a positive prediction
+        else: 
+            neg += 1        # counts the number of tweets with a negativee prediction 
+    score = (pos - neg) /numOfTweets     # calculates the score 
+
+    # sets the score of the result
+    result['score'] = score
+
+    mostNeg = 0
+    mostPos = 0
+    index = 0
+
+    for x in data['results']:
+        if x['probability'] > mostPos:   # if the probability for positive is greater than the current mosPos 
+            mostPos = x['probability']   # set mostPos to current probablity 
+            mostPosIndex = index            # sets the index
+        if x['probability'] < mostNeg:   # if the probability for negative is greater than the current mostNeg 
+            mostNeg = x['probability']   # set mostNeg to current probability 
+            mostNegIndex = index            # sets the index
+        index += 1                      # increments the index 
+
+    # sets the results posIndex and negIndex values 
+    result['posIndex'] = mostPosIndex
+    result['negIndex'] = mostNegIndex
+
+    return result
--- ts/app/routes.py
+++ ts/app/routes.py
@@ -1,6 +1,13 @@
 from flask import render_template, flash, redirect
 from app import application
 from app.forms import SearchForm
+from .TweepyFunctions import *
+from .Credentials import *
+import pymongo
+from pymongo import MongoClient
+import json 
+import boto3
+from datetime import datetime
 
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index/', methods=['GET', 'POST'])
@@ -9,11 +16,77 @@ def home():
 
     if form.validate_on_submit():
         flash('Query {}'.format(form.query.data))
-        # TODO Add tweepy code
+
+        runtime = boto3.client(
+        'sagemaker-runtime',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name='us-east-1'
+        )
+
+        # get tweets
+        if(form.submitUsername.data):
+            search_type = 'user'
+            tweets = get_users_tweets(form.query.data)
+        else:
+            search_type = 'hashtag'
+            tweets = get_hashtag_tweets(form.query.data)
+
+        #if rate limit was exceeded print message
+        if tweets == RATE_ERROR:
+            print("Rate limit reached. Please wait before trying again")
+
+        #if search error occured print message
+        elif tweets == SEARCH_ERROR:
+            print("The username searched is either private or does not exist")
+
+        #if no tweets exist
+        elif not tweets:
+            print("There are no avalible tweets for that user or hashtag")
+
+        else:
+            processedTweets = []
+            #send each tweet to preprocess function
+            for x in tweets['tweet']:
+                processedTweets.append(preprocess(x))
+
+            processedTweet = {"tweet": processedTweets}
+      
+            #call sentiment alg with processedTweet
+            response = runtime.invoke_endpoint(
+            EndpointName='twitter-svc-tuner-200324-1449-015-0d842270',
+            Body=json.dumps(processedTweet),
+            ContentType='application/json')
+
+            results = json.loads(response['Body'].read())
+
+            #aggregates the results into a result object with score, posIndex, and negIndex
+            result = aggregateData(results)
+
+             #creates database entry
+            now = datetime.now()
+            post = {
+                "query": form.query.data,
+                "type" : search_type,
+                "score" : result['score'],
+                "mostPositive" : tweets['tweet'][result['posIndex']],
+                "mostNegative" : tweets['tweet'][result['negIndex']],
+                "timeLog" : now
+             }
+            cluster = MongoClient(database_connect)
+            db = cluster["DSS"]
+            collection = db["Search_Records"]
+            #comment this line out if you dont want to log the results in the database
+            collection.insert_one(post)
 
         # Use flash messages to display validation errors and stuff
-
-        return render_template('index.html', form=form, data=form.query.data)
+            return render_template('index.html',
+                                 form=form, 
+                                 data=form.query.data, 
+                                 score=result['score'], 
+                                 posTweet = tweets['tweet'][result['posIndex']],
+                                 negTweet =tweets['tweet'][result['negIndex']]
+                                 )
 
 
     return render_template('index.html', form=form)
